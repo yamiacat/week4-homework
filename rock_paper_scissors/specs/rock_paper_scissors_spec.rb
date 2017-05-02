be5919c2e8d6144ff4fce2f67a90fd6d5996b0cd
Matt Rose <yamiacat@gmail.com>
2017-05-02 09:19:42 +0100

    Add test fixes to compensate for changed game language

--- rock_paper_scissors/specs/rock_paper_scissors_spec.rb
+++ rock_paper_scissors/specs/rock_paper_scissors_spec.rb
@@ -6,28 +6,28 @@ class TestRockPaperScissors < MiniTest::Test
 
   def test_horse_gets_error_message
     game = RockPaperScissors.new("horse", "rock")
-    error = "Only 'rock', 'paper', and 'scissors' are valid throws!"
+    error = "Only 'rock', 'paper', and 'scissors' are valid launches!"
     assert_equal(error, game.result)
   end
 
   def test_paper_beats_rock
     game = RockPaperScissors.new("rock", "paper")
-    assert_equal("Player 2 wins with paper!", game.result)
+    assert_equal("Player 2 ANNIHILATES Player 1 with paper!", game.result)
   end
 
   def test_scissors_beats_paper
     game = RockPaperScissors.new("scissors", "paper")
-    assert_equal("Player 1 wins with scissors!", game.result)
+    assert_equal("Player 1 VAPORISES Player 2 with scissors!", game.result)
   end
 
   def test_rock_beats_scissors
     game = RockPaperScissors.new("scissors", "rock")
-    assert_equal("Player 2 winds with rock!", game.result)
+    assert_equal("Player 2 ANNIHILATES Player 1 with rock!", game.result)
   end
 
   def test_draws_draw
     game = RockPaperScissors.new("scissors", "scissors")
-    assert_equal("Draw! Throw again!", game.result)
+    assert_equal("MUTUALLY ASSURED DESTRUCTION! Launch again!", game.result)
   end
 
 end
